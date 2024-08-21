0cffd732718d206cd6d52a1313ff387403a29bbe
Arpan Halder <iamthearpan@gmail.com>
2024-08-21 13:46:28 +0530

    Connect mongo db and change app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,22 @@
-import express from 'express'
+import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
 app.use(cors())
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
+
+
+//routes declaration
+
+
+
+export { app }
